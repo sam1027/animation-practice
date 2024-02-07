657e396180caa04764c8c2dab6133fd86f9fa40d
Seongeun Yu <yse1027@gmail.com>
2024-02-07 16:19:50 +0900

    framer-motion 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import {motion, AnimatePresence} from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  position: absolute;
+  top: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVars = {
+  initial: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+    
+  },
+  leaving: {
+    opacity: 0,
+    scale: 0,
+    y: 50,
+  },
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const toggleShowing = () => setShowing(prev => !prev);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <button onClick={toggleShowing}>click</button>
+      <AnimatePresence>
+        {showing ? 
+        <Box 
+          variants={boxVars}
+          initial="initial"
+          animate="visible"
+          exit="leaving"
+        /> 
+        : null}
+      </AnimatePresence>
+    </Wrapper>
   );
 }
 
